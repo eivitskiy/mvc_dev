712611e5947f6f041e9b2673bee7b08eaf24fbd0
eivitskiy <e_ivitskiy@mail.ru>
2017-06-05 09:52:45 +0700

    Remove comments

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: eivitskiy
- * Date: 03.06.17
- * Time: 11:00
- */
 
 define("APPPATH", $_SERVER['DOCUMENT_ROOT'].'/application/');
 
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);*/
 
 require_once 'core/load.php';
